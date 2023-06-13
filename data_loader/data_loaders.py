fcdfa576fe4e311a82fca79f8dbc75bce12f2d57
asdftyui <bbb6254177@gmail.com>
2023-06-13 07:32:01 +0000

    #4 feat: add data loader for NFM

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,16 +1,71 @@
-from torchvision import datasets, transforms
+from torch.utils.data import Dataset, DataLoader
+import torch
+import pandas as pd
+import os
+import ast
+
 from base import BaseDataLoader
+from .data_preprocess import cars_preprocess
 
 
-class MnistDataLoader(BaseDataLoader):
-    """
-    MNIST data loading demo using BaseDataLoader
-    """
-    def __init__(self, data_dir, batch_size, shuffle=True, validation_split=0.0, num_workers=1, training=True):
-        trsfm = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize((0.1307,), (0.3081,))
-        ])
-        self.data_dir = data_dir
-        self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
-        super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+class CARSDataset(Dataset):
+    def __init__(self, data, max_seq_len, type="train"):
+        self.data = data
+        self.max_seq_len = max_seq_len
+        self.type = type
+        
+        if self.type == "train":
+            self.X = self.data.drop('rating', axis=1)
+            self.y = self.data.rating
+        else:
+            self.X = self.data
+    
+    def __getitem__(self, index):
+        row = self.X.loc[index]
+        
+        user, item, year = row[0], row[1], row[5]
+        director, writer, genre = self.pad_sequence(ast.literal_eval(row[2])), self.pad_sequence(ast.literal_eval(row[3])), self.pad_sequence(ast.literal_eval(row[4]))
+        
+        if self.type == "train":
+            return torch.IntTensor([user, item] + director + writer + genre + [year]), self.y.loc[index]
+        else:
+            return torch.IntTensor([user, item] + director + writer + genre + [year])
+    
+    def __len__(self):
+        return len(self.data)
+    
+    def pad_sequence(self, att_list):
+        seq = [0] * self.max_seq_len
+        if len(att_list) > self.max_seq_len:
+            seq = att_list[:self.max_seq_len]
+        else:
+            seq[:len(att_list)] = att_list
+        
+        return seq
+    
+    
+class CARSDataLoader(DataLoader):
+    def __init__(self, data_dir, batch_size, max_seq_len, shuffle=False, num_workers=1):
+        
+        self.train, self.valid = self.load_context_data(data_dir)
+        self.train_dataset = CARSDataset(self.train, max_seq_len, "train")
+        self.test_dataset = CARSDataset(self.valid, max_seq_len, "train")
+        
+        self.init_kwargs = {
+            'batch_size': batch_size,
+            'shuffle': shuffle,
+            'num_workers': num_workers
+        }
+        
+        super().__init__(self.train_dataset, **self.init_kwargs)
+        
+        
+    def load_context_data(self, data_dir):
+        
+        if not os.path.exists(os.path.join(data_dir, "context_train_data.csv")):
+            cars_preprocess(data_dir)
+        
+        return pd.read_csv(os.path.join(data_dir, "context_train_data.csv")), pd.read_csv(os.path.join(data_dir, "context_valid_data.csv"))
+    
+    def split_validation(self):
+        return DataLoader(self.test_dataset, **self.init_kwargs)
