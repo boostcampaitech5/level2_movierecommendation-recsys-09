51724235dccd3b916aec32519ff2b8ccbd662ac2
asdftyui <bbb6254177@gmail.com>
2023-06-13 07:33:17 +0000

    #4 feat: add NFM model

--- model/model.py
+++ model/model.py
@@ -1,22 +1,213 @@
 import torch.nn as nn
 import torch.nn.functional as F
-from base import BaseModel
+import torch
+import os
+
+os.environ['CUDA_LAUNCH_BLOCKING'] = "1"
+
+def activation_layer(act_name):
+    """Construct activation layers
+
+    Args:
+        act_name: str or nn.Module, name of activation function
+        hidden_size: int, used for Dice activation
+        dice_dim: int, used for Dice activation
+    Return:
+        act_layer: activation layer
+    """
+    if isinstance(act_name, str):
+        if act_name.lower() == 'sigmoid':
+            act_layer = nn.Sigmoid()
+        elif act_name.lower() == 'relu':
+            act_layer = nn.ReLU(inplace=True)
+        elif act_name.lower() == 'prelu':
+            act_layer = nn.PReLU()
+    elif issubclass(act_name, nn.Module):
+        act_layer = act_name()
+    else:
+        raise NotImplementedError
+
+    return act_layer
 
 
-class MnistModel(BaseModel):
-    def __init__(self, num_classes=10):
-        super().__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, num_classes)
+class DNN(nn.Module):
+    """The Multi Layer Percetron
 
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x, dim=1)
+      Input shape
+        - nD tensor with shape: ``(batch_size, ..., input_dim)``. The most common situation would be a 2D input with shape ``(batch_size, input_dim)``.
+
+      Output shape
+        - nD tensor with shape: ``(batch_size, ..., hidden_size[-1])``. For instance, for a 2D input with shape ``(batch_size, input_dim)``, the output would have shape ``(batch_size, hidden_size[-1])``.
+
+      Arguments
+        - **inputs_dim**: input feature dimension.
+
+        - **hidden_units**:list of positive integer, the layer number and units in each layer.
+
+        - **activation**: Activation function to use.
+
+        - **l2_reg**: float between 0 and 1. L2 regularizer strength applied to the kernel weights matrix.
+
+        - **dropout_rate**: float in [0,1). Fraction of the units to dropout.
+
+        - **use_bn**: bool. Whether use BatchNormalization before activation or not.
+
+        - **seed**: A Python integer to use as random seed.
+    """
+
+    def __init__(self, inputs_dim, hidden_units, activation='relu', l2_reg=0, dropout_rate=0, use_bn=False,
+                 init_std=0.0001, device='cpu'):
+        super(DNN, self).__init__()
+        self.dropout_rate = dropout_rate
+        self.dropout = nn.Dropout(dropout_rate)
+        self.l2_reg = l2_reg
+        self.use_bn = use_bn
+        if len(hidden_units) == 0:
+            raise ValueError("hidden_units is empty!!")
+        hidden_units = [inputs_dim] + list(hidden_units)
+
+        self.linears = nn.ModuleList(
+            [nn.Linear(hidden_units[i], hidden_units[i + 1]) for i in range(len(hidden_units) - 1)])
+
+        if self.use_bn:
+            self.bn = nn.ModuleList(
+                [nn.BatchNorm1d(hidden_units[i + 1]) for i in range(len(hidden_units) - 1)])
+
+        self.activation_layers = nn.ModuleList(
+            [activation_layer(activation) for i in range(len(hidden_units) - 1)])
+
+        for name, tensor in self.linears.named_parameters():
+            if 'weight' in name:
+                nn.init.normal_(tensor, mean=0, std=init_std)
+
+        self.to(device)
+
+    def forward(self, inputs):
+        deep_input = inputs
+
+        for i in range(len(self.linears)):
+
+            fc = self.linears[i](deep_input)
+
+            if self.use_bn:
+                fc = self.bn[i](fc)
+
+            fc = self.activation_layers[i](fc)
+
+            fc = self.dropout(fc)
+            deep_input = fc
+        return deep_input
+
+
+class BiInteractionPooling(nn.Module):
+    """Bi-Interaction Layer used in Neural FM,compress the
+     pairwise element-wise product of features into one single vector.
+
+      Input shape
+        - A 3D tensor with shape:``(batch_size,field_size,embedding_size)``.
+
+      Output shape
+        - 3D tensor with shape: ``(batch_size,1,embedding_size)``.
+
+      References
+        - [He X, Chua T S. Neural factorization machines for sparse predictive analytics[C]//Proceedings of the 40th International ACM SIGIR conference on Research and Development in Information Retrieval. ACM, 2017: 355-364.](http://arxiv.org/abs/1708.05027)
+    """
+
+    def __init__(self):
+        super(BiInteractionPooling, self).__init__()
+
+    def forward(self, inputs):
+        concated_embeds_value = inputs
+        square_of_sum = torch.pow(
+            torch.sum(concated_embeds_value, dim=1, keepdim=True), 2)
+        sum_of_square = torch.sum(
+            concated_embeds_value * concated_embeds_value, dim=1, keepdim=True)
+        cross_term = 0.5 * (square_of_sum - sum_of_square)
+        return cross_term
+
+
+class NFMmodel(nn.Module):
+    """Instantiates the NFM Network architecture.
+
+    :param linear_feature_columns: An iterable containing all the features used by linear part of the model.
+    :param dnn_feature_columns: An iterable containing all the features used by deep part of the model.
+    :param dnn_hidden_units: list,list of positive integer or empty list, the layer number and units in each layer of deep net
+    :param l2_reg_embedding: float. L2 regularizer strength applied to embedding vector
+    :param l2_reg_linear: float. L2 regularizer strength applied to linear part.
+    :param l2_reg_dnn: float . L2 regularizer strength applied to DNN
+    :param init_std: float,to use as the initialize std of embedding vector
+    :param seed: integer ,to use as random seed.
+    :param biout_dropout: When not ``None``, the probability we will drop out the output of BiInteractionPooling Layer.
+    :param dnn_dropout: float in [0,1), the probability we will drop out a given DNN coordinate.
+    :param dnn_activation: Activation function to use in deep net
+    :param task: str, ``"binary"`` for  binary logloss or  ``"regression"`` for regression loss
+    :param device: str, ``"cpu"`` or ``"cuda:0"``
+    :param gpus: list of int or torch.device for multiple gpus. If None, run on `device`. `gpus[0]` should be the same gpu with `device`.
+    :return: A PyTorch model instance.
+
+    """
+
+    def __init__(self, **params):
+        super(NFMmodel, self).__init__()
+        
+        self.device = params['device']
+        
+        self.user_embedding = nn.Embedding(params['n_users'], params['embed_dim'])
+        self.item_embedding = nn.Embedding(params['n_items'], params['embed_dim'])
+        self.director_embedding = nn.Embedding(params['n_directors'], params['embed_dim'])
+        self.writer_embedding = nn.Embedding(params['n_writers'], params['embed_dim'])
+        self.genre_embedding = nn.Embedding(params['n_genres'], params['embed_dim'])
+        
+        self.linear = nn.Linear(params['max_seq_len']*3 + 3, 1, bias=True)
+        
+        self.regularization_weight = []
+
+        self.dnn = DNN( params['embed_dim']+1,
+                       params['dnn_hidden_units'],
+                       activation=params['dnn_activation'], l2_reg=params['l2_reg_dnn'], dropout_rate=params['dnn_dropout'], use_bn=params['use_bn'],
+                       init_std=params['init_std'], device=self.device)
+        self.dnn_linear = nn.Linear(
+            params['dnn_hidden_units'][-1], 1, bias=False).to(self.device)
+        self.add_regularization_weight(
+            filter(lambda x: 'weight' in x[0] and 'bn' not in x[0], self.dnn.named_parameters()), l2=params['l2_reg_dnn'])
+        self.add_regularization_weight(self.dnn_linear.weight, l2=params['l2_reg_dnn'])
+        self.bi_pooling = BiInteractionPooling()
+        self.bi_dropout = params['bi_dropout']
+        if self.bi_dropout > 0:
+            self.dropout = nn.Dropout(params['bi_dropout'])
+        self.to(self.device)
+
+    def add_regularization_weight(self, weight_list, l1=0.0, l2=0.0):
+        # For a Parameter, put it in a list to keep Compatible with get_regularization_loss()
+        if isinstance(weight_list, torch.nn.parameter.Parameter):
+            weight_list = [weight_list]
+        # For generators, filters and ParameterLists, convert them to a list of tensors to avoid bugs.
+        # e.g., we can't pickle generator objects when we save the model.
+        else:
+            weight_list = list(weight_list)
+        self.regularization_weight.append((weight_list, l1, l2))
+
+    def forward(self, X):
+        
+        user_embed = self.user_embedding(X[:, 0])
+        item_embed = self.item_embedding(X[:, 1])
+        director_embed = self.director_embedding(X[:, 2:7])
+        writer_embed = self.writer_embedding(X[:, 7:12])
+        genre_embed = self.genre_embedding(X[:, 12:17])
+        year = X[:, 17]
+        
+        linear_logit = self.linear(X.float())
+        fm_input = torch.cat((user_embed.unsqueeze(1), item_embed.unsqueeze(1), director_embed, writer_embed, genre_embed), dim=1)
+        bi_out = self.bi_pooling(fm_input)
+        if self.bi_dropout > 0:
+            bi_out = self.dropout(bi_out)
+
+        dnn_input = torch.cat((torch.flatten(bi_out, start_dim=1), year.unsqueeze(1)), dim=-1)
+        dnn_output = self.dnn(dnn_input)
+        dnn_logit = self.dnn_linear(dnn_output)
+
+        logit = linear_logit + dnn_logit
+
+        y_pred = torch.sigmoid(logit)
+
+        return y_pred
