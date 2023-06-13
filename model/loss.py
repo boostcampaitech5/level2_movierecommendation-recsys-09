60d0f5df0d48d686c97b4a6f2c9823028d4b1f37
asdftyui <bbb6254177@gmail.com>
2023-06-13 07:33:34 +0000

    #4 feat: add BCE loss for NFM

--- model/loss.py
+++ model/loss.py
@@ -1,5 +1,5 @@
 import torch.nn.functional as F
 
 
-def nll_loss(output, target):
-    return F.nll_loss(output, target)
+def BCE_loss(output, target):
+    return F.binary_cross_entropy(output.squeeze(1), target.float())
