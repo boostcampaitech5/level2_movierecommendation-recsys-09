1d871b558f9506783a560113a9009476c43e8c03
asdftyui <bbb6254177@gmail.com>
2023-06-14 08:18:49 +0000

    #4 feat: add test code for 2 step model

--- test.py
+++ test.py
@@ -1,76 +1,65 @@
 import argparse
 import torch
-from tqdm import tqdm
-import data_loader.data_loaders as module_data
-import model.loss as module_loss
-import model.metric as module_metric
+import pandas as pd
+import numpy as np
+import os
+from torch.utils.data import DataLoader
+from data_loader.data_preprocess import cars_test_preprocess, indexing, unindexing
+from data_loader.data_loaders import CARSDataset
 import model.model as module_arch
 from parse_config import ConfigParser
+from utils import prepare_device
 
 
 def main(config):
-    logger = config.get_logger('test')
-
-    # setup data_loader instances
-    data_loader = getattr(module_data, config['data_loader']['type'])(
-        config['data_loader']['args']['data_dir'],
-        batch_size=512,
-        shuffle=False,
-        validation_split=0.0,
-        training=False,
-        num_workers=2
-    )
-
+    
+    device, _ = prepare_device(config['n_gpu'])
+    
+    data_path = os.path.join(config['test']['data_dir'], "context_test_data.csv")
+    
+    if not os.path.exists(data_path):
+        cars_test_preprocess(config['test']['data_dir'])
+    
+    data = pd.read_csv(data_path)
+    
     # build model architecture
     model = config.init_obj('arch', module_arch)
-    logger.info(model)
-
-    # get function handles of loss and metrics
-    loss_fn = getattr(module_loss, config['loss'])
-    metric_fns = [getattr(module_metric, met) for met in config['metrics']]
-
-    logger.info('Loading checkpoint: {} ...'.format(config.resume))
-    checkpoint = torch.load(config.resume)
-    state_dict = checkpoint['state_dict']
-    if config['n_gpu'] > 1:
-        model = torch.nn.DataParallel(model)
-    model.load_state_dict(state_dict)
-
-    # prepare model for testing
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = model.to(device)
+    model_path = config['test']['model_dir']
+    model.load_state_dict(torch.load(model_path)['state_dict'])
+    model.to(device)
     model.eval()
+    
+    pred_list = []
+    test_dataset = CARSDataset(data, config['data_loader']['args']['max_seq_len'], "test")
+    test_dataloader = DataLoader(test_dataset, batch_size=config['test']['batch_size'],  shuffle=False)
 
-    total_loss = 0.0
-    total_metrics = torch.zeros(len(metric_fns))
+    for data in test_dataloader:
+        rating_pred = model(data.to(device))
+        rating_pred = rating_pred.cpu().data.numpy().copy()
+        ind = np.argpartition(rating_pred.squeeze(1), -10)[-10:]
 
-    with torch.no_grad():
-        for i, (data, target) in enumerate(tqdm(data_loader)):
-            data, target = data.to(device), target.to(device)
-            output = model(data)
-
-            #
-            # save sample images, or do something with output here
-            #
-
-            # computing loss, metrics on test set
-            loss = loss_fn(output, target)
-            batch_size = data.shape[0]
-            total_loss += loss.item() * batch_size
-            for i, metric in enumerate(metric_fns):
-                total_metrics[i] += metric(output, target) * batch_size
-
-    n_samples = len(data_loader.sampler)
-    log = {'loss': total_loss / n_samples}
-    log.update({
-        met.__name__: total_metrics[i].item() / n_samples for i, met in enumerate(metric_fns)
-    })
-    logger.info(log)
+        pred_list.append(data[ind, 1])
+    
+    first_row = True
+    recommend_df = pd.DataFrame()
+    for i, items in enumerate(pred_list):
+        user_rec_df = pd.DataFrame({'user':[i]*len(items), 'item':items})
+        if first_row == True:
+            recommend_df = user_rec_df
+            first_row = False
+        else:
+            recommend_df = pd.concat([recommend_df, user_rec_df], axis = 0, sort=False)
+    
+    rating_df = pd.read_csv(os.path.join(config['test']['data_dir'], "train_ratings.csv"))
+    for column in ['user', 'item']:
+        recommend_df = unindexing(rating_df, recommend_df, column)
+    
+    recommend_df.to_csv(os.path.join(config['test']['submission_dir'], "nfm_submission.csv"), index=False)
 
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser(description='PyTorch Template')
-    args.add_argument('-c', '--config', default=None, type=str,
+    args.add_argument('-c', '--config', default='cars_config.json', type=str,
                       help='config file path (default: None)')
     args.add_argument('-r', '--resume', default=None, type=str,
                       help='path to latest checkpoint (default: None)')
