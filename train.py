6cfe700100275d37516635aed6fa8e258279534b
asdftyui <bbb6254177@gmail.com>
2023-06-15 12:42:55 +0000

    #10 feat: add wandb for hyperparameter tuning

--- train.py
+++ train.py
@@ -2,13 +2,18 @@ import argparse
 import collections
 import torch
 import numpy as np
+import wandb
+import sweep_config
 import data_loader.data_loaders as module_data
 import model.loss as module_loss
 import model.metric as module_metric
 import model.model as module_arch
 from parse_config import ConfigParser
+from operator import getitem
+from functools import reduce
 from trainer import Trainer
 from utils import prepare_device
+import functools
 
 
 # fix random seeds for reproducibility
@@ -18,7 +23,33 @@ torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 np.random.seed(SEED)
 
+
+def update_config(config, modification):
+    if modification is None:
+        return config
+
+    for k, v in modification.items():
+        if v is not None:
+            _set_by_path(config, k, v)
+    return config
+
+def _set_by_path(tree, keys, value):
+    """Set a value in a nested object in tree by sequence of keys."""
+    keys = keys.split(';')
+    _get_by_path(tree, keys[:-1])[keys[-1]] = value
+
+def _get_by_path(tree, keys):
+    """Access a nested object in tree by sequence of keys."""
+    return reduce(getitem, keys, tree)
+
+
 def main(config):
+
+    wandb.init(project=config['name'], entity = "ffm", config=sweep_config.default)
+    
+    modification = wandb.config
+    update_config(config, modification)
+
     logger = config.get_logger('train')
 
     # setup data_loader instances
@@ -56,7 +87,7 @@ def main(config):
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser(description='PyTorch Template')
-    args.add_argument('-c', '--config', default=None, type=str,
+    args.add_argument('-c', '--config', default='gcn_config.json', type=str,
                       help='config file path (default: None)')
     args.add_argument('-r', '--resume', default=None, type=str,
                       help='path to latest checkpoint (default: None)')
@@ -69,5 +100,13 @@ if __name__ == '__main__':
         CustomArgs(['--lr', '--learning_rate'], type=float, target='optimizer;args;lr'),
         CustomArgs(['--bs', '--batch_size'], type=int, target='data_loader;args;batch_size')
     ]
+
     config = ConfigParser.from_args(args, options)
-    main(config)
+
+    sweep_id = wandb.sweep(
+        sweep=sweep_config.sweep_configuration,
+        entity="ffm",
+        project=config['name']
+    )
+    
+    wandb.agent(sweep_id=sweep_id, function=functools.partial(main, config), count=10)
