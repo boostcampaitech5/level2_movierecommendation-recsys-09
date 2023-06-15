5ea8572e79d6fb5e6395edc538fd4de66ddc39f2
daream2 <daream2@naver.com>
2023-06-15 05:47:47 +0000

    #5 feat: add data loader for BERT4Rec

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,5 +1,8 @@
 from torchvision import datasets, transforms
 from base import BaseDataLoader
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
 
 
 class MnistDataLoader(BaseDataLoader):
@@ -14,3 +17,53 @@ class MnistDataLoader(BaseDataLoader):
         self.data_dir = data_dir
         self.dataset = datasets.MNIST(self.data_dir, train=training, download=True, transform=trsfm)
         super().__init__(self.dataset, batch_size, shuffle, validation_split, num_workers)
+
+
+class SeqDataset(Dataset):
+    def __init__(self, user_train, num_user, num_item, max_len, mask_prob):
+        self.user_train = user_train
+        self.num_user = num_user
+        self.num_item = num_item
+        self.max_len = max_len
+        self.mask_prob = mask_prob
+
+    def __len__(self):
+        # 총 user의 수 = 학습에 사용할 sequence의 수
+        return self.num_user
+
+    def __getitem__(self, user): 
+        # iterator를 구동할 때 사용됩니다.
+        seq = self.user_train[user]
+        tokens = []
+        labels = []
+        for s in seq:
+            prob = np.random.random()
+            if prob < self.mask_prob:
+                prob /= self.mask_prob
+
+                # BERT 학습
+                if prob < 0.8:
+                    # masking
+                    tokens.append(self.num_item + 1)  # mask_index: num_item + 1, 0: pad, 1~num_item: item index
+                elif prob < 0.9:
+                    tokens.append(np.random.randint(1, self.num_item+1))  # item random sampling
+                else:
+                    tokens.append(s)
+                labels.append(s)  # 학습에 사용
+            else:
+                tokens.append(s)
+                labels.append(0)  # 학습에 사용 X, trivial
+        tokens = tokens[-self.max_len:]
+        labels = labels[-self.max_len:]
+        mask_len = self.max_len - len(tokens)
+
+        # zero padding
+        tokens = [0] * mask_len + tokens
+        labels = [0] * mask_len + labels
+        return torch.LongTensor(tokens), torch.LongTensor(labels)
+    
+class BERT4RecDataLoader(DataLoader):
+    def __init__(self, user_train, num_user, num_item, max_len, mask_prob, batch_size):
+        self.seq_dataset = SeqDataset(user_train, num_user, num_item, max_len, mask_prob)
+        
+        super().__init__(self.seq_dataset, batch_size=batch_size, shuffle=True, pin_memory=True)
