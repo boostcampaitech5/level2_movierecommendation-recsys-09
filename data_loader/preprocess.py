0ce00335cfa4a7dffd7a344560332729f9d9b36a
daream2 <daream2@naver.com>
2023-06-15 06:39:57 +0000

    #5 feat: add preprocess for BERT4Rec

---
+++ data_loader/preprocess.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+import os
+from collections import defaultdict
+
+def BERT4Rec_preprocess(df):
+    item_ids = df['item'].unique() #영화 리스트
+    user_ids = df['user'].unique() #사용자 리스트
+
+    # user, item indexing
+    item2idx = pd.Series(data=np.arange(len(item_ids))+1, index=item_ids) # item re-indexing (1~num_item), num_item+1: mask idx -> 0은 패딩된 아이템을 의미하게끔
+    user2idx = pd.Series(data=np.arange(len(user_ids)), index=user_ids) # user re-indexing (0~num_user-1)
+
+    # dataframe indexing
+    df = pd.merge(df, pd.DataFrame({'item': item_ids, 'item_idx': item2idx[item_ids].values}), on='item', how='inner')
+    df = pd.merge(df, pd.DataFrame({'user': user_ids, 'user_idx': user2idx[user_ids].values}), on='user', how='inner')
+    df.sort_values(['user_idx', 'time'], inplace=True)
+    del df['item'], df['user'] 
+
+    # train set, valid set 생성
+    users = defaultdict(list) # defaultdict은 dictionary의 key가 없을때 default 값을 value로 반환
+    user_train = {}
+    user_valid = {}
+    for u, i, t in zip(df['user_idx'], df['item_idx'], df['time']):
+        users[u].append(i)
+
+    for user in users:
+        user_train[user] = users[user][:-1]
+        user_valid[user] = [users[user][-1]] #user가 본 마지막 영화를 valid로
+    
+    return user_train, user_valid
